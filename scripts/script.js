4b5bbed374f0dba072cc106ac2e80947f4128787
Marc Schaefer <hi@marcschaefer.de>
2021-08-23 21:28:02 +0200

    Add first version of portfolio slider

--- scripts/script.js
+++ scripts/script.js
@@ -54,3 +54,57 @@ window.onload = function() {
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
 };
+
+const IMG_URLS = [
+  "https://preview.redd.it/9k1j9k0axpi71.png?width=960&crop=smart&auto=webp&s=7f8dd0b0202b708a1a4c1a5acd126af1776ca755",
+  "https://preview.redd.it/6x4o8fshhvi71.png?width=960&crop=smart&auto=webp&s=463086bc80f149144d9f1fecd24e4e481149ba1b",
+  "https://external-preview.redd.it/eRohIMX0_gZlmiSLTEDyRUVmw7V9WGLbn4I3RPY7cNs.png?width=960&crop=smart&auto=webp&s=a1ef1b0c05101f21ef729e02d04cb174f84ceaea"
+];
+
+let currentPos = 0;
+
+document.getElementById("next").addEventListener("click", loadNextImage);
+document.getElementById("prev").addEventListener("click", loadPrevImage);
+document.getElementById("showcase").addEventListener("animationend", removeClass);
+
+// wie macht man das mit variablen argumenten und this?
+
+hideControls();
+
+function loadNextImage() {
+  if (currentPos < IMG_URLS.length - 1) {
+    document.getElementById("showcase").style.background = `url(${IMG_URLS[++currentPos]})`;
+    document.getElementById("showcase").classList.add("rotate");
+    document.getElementById("showcase-description").classList.add("rotate");
+  }
+ 
+  hideControls();
+}
+
+function loadPrevImage() {
+  if (currentPos > 0) {
+    document.getElementById("showcase").style.background = `url(${IMG_URLS[--currentPos]})`;
+    document.getElementById("showcase").classList.add("rotate");
+    document.getElementById("showcase-description").classList.add("rotate");
+  }
+  
+  hideControls();
+}
+
+function removeClass() {
+  document.getElementById("showcase").classList.remove("rotate");
+  document.getElementById("showcase-description").classList.remove("rotate");
+}
+
+function hideControls() {
+  document.getElementById("next").style.visibility = "visible";
+  document.getElementById("prev").style.visibility = "visible";
+  
+  if (currentPos === IMG_URLS.length - 1) {
+    document.getElementById("next").style.visibility = "hidden";
+  } 
+  
+  if (currentPos === 0) {
+    document.getElementById("prev").style.visibility = "hidden";
+  }
+}
